0fd05a7c614d54fe835729c88ec7980d978e146d
Gonzalo Lopez <gonzapolitec@gmail.com>
2026-09-02 21:32:12 -0300

    add domain (falta agregar user)

--- backend/src/main/java/com/ovaltrack/backend/user/domain/User.java
+++ backend/src/main/java/com/ovaltrack/backend/user/domain/User.java
@@ -1,4 +1,20 @@
 package com.ovaltrack.backend.user.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 }
