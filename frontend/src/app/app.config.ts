5cbaf38bc34a91f12984be5e16aa303cbb6894bd
Manuel Joaquin Rocha <manuel05rocha@gmail.com>
2026-09-15 19:28:37 -0300

    refactor: migrate to Dexie for local database management and update match selection logic

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideHttpClient, withInterceptors } from '@angular/common/http'; // 👈 Importar withInterceptors
 import { jwtInterceptor } from './core/interceptor/jwt.interceptor';          // 👈 Importar jwtInterceptor
+import { initializeLocalDatabases } from './features/cargaEnVivo/data/local-databases';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +14,11 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
     }),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => initializeLocalDatabases(),
+      multi: true,
+    },
     provideHttpClient(withInterceptors([jwtInterceptor])) // 👈 Registrar el interceptor aquí
   ]
 };
