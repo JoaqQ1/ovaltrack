f004f419444f0f8b2853fa42cec8c9e09381e06d
AverageZeko <lizandro.e.marino@gmail.com>
2026-09-23 23:39:11 -0300

    feat: added function getByDivisionId() to fetch divisionPlayers and remove() to disassociate players from division

--- frontend/src/app/services/division-player.service.ts
+++ frontend/src/app/services/division-player.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
+import { DivisionPlayerResponse } from '../features/division-player/type/division-player.types';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,16 @@ export class DivisionPlayerService {
 
   registerPlayer(playerData: any): Observable<any> {
     return this.http.post<any>(this.apiUrl, playerData);
+  }
+
+  getByDivisionId(divisionId: string): Observable<DivisionPlayerResponse[]> {
+    const params = new HttpParams().set('divisionId', divisionId);
+    return this.http.get<DivisionPlayerResponse[]>(this.apiUrl, { params });
+  }
+
+  remove(divisionPlayerId: string): Observable<string> {
+    return this.http.delete<string>(`${this.apiUrl}/${divisionPlayerId}`, {
+      responseType: 'text' as 'json'
+    });
   }
 }
