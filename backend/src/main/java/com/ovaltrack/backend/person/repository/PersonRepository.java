a67ecec33c24ccf51dbd8e0c531a77e29f99e850
AverageZeko <lizandro.e.marino@gmail.com>
2026-09-24 19:31:10 -0300

    feat: added query to find every player in a club (also added paginated version of it)

--- backend/src/main/java/com/ovaltrack/backend/person/repository/PersonRepository.java
+++ backend/src/main/java/com/ovaltrack/backend/person/repository/PersonRepository.java
@@ -1,8 +1,14 @@
 package com.ovaltrack.backend.person.repository;
 
 import com.ovaltrack.backend.person.domain.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -18,4 +24,26 @@ public interface PersonRepository extends JpaRepository<Person, UUID> {
     boolean existsByContactPhone(String contactPhone);
 
     List<Person> findAllByClubId(UUID clubId);
+
+    @Query("""
+        SELECT DISTINCT p
+        FROM Person p
+        JOIN DivisionPlayer dp ON dp.person.id = p.id
+        JOIN dp.division d
+        JOIN d.club c
+        WHERE c.id = :clubId
+        ORDER BY p.lastName, p.firstName
+    """)
+    Collection<Person> findPlayersByClubId(@Param("clubId") UUID clubId);
+
+    @Query("""
+        SELECT DISTINCT p
+        FROM Person p
+        JOIN DivisionPlayer dp ON dp.person.id = p.id
+        JOIN dp.division d
+        JOIN d.club c
+        WHERE c.id = :clubId
+        ORDER BY p.lastName, p.firstName
+    """)
+    Page<Person> findPaginatedPlayersByClubId(@Param("clubId") UUID clubId, Pageable pageable);
 }
