98b2444bb53d3335cb23aa467af8971b812ab6ea
Joaquin Quiroga <quirogajuaco7@gmail.com>
2026-09-25 13:20:47 -0300

    fix(User): Correccion de estados de la cuenta iniciales

--- backend/src/main/java/com/ovaltrack/backend/user/domain/User.java
+++ backend/src/main/java/com/ovaltrack/backend/user/domain/User.java
@@ -37,14 +37,13 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(name = "account_status")
     @Builder.Default
-    private UserStatus accountStatus = UserStatus.ACTIVE;
+    private UserStatus accountStatus = UserStatus.PENDING_APPROVAL;
 
     @OneToOne(optional = false)
     @JoinColumn(name = "person_id", nullable = false, unique = true)
     private Person person;
 
-    @Builder.Default
-    private Boolean active = true;
+    private Boolean active;
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
