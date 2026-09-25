5e1d387a520c3557e0ac72c882090b2ee26629c2
Joaquin Quiroga <quirogajuaco7@gmail.com>
2026-09-25 14:00:26 -0300

    feat(auth): Creacion de estados de una solicitud de registro

---
+++ backend/src/main/java/com/ovaltrack/backend/auth/registration/domain/RegistrationRequestStatus.java
@@ -0,0 +1,9 @@
+package com.ovaltrack.backend.auth.registration.domain;
+
+public enum RegistrationRequestStatus {
+    PENDING,
+    APPROVED,
+    REJECTED,
+    CANCELLED,
+    NEEDS_INFORMATION
+}
